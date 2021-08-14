0f615136ef3b5f85b378429497fc4089c6afd450
hschne <hans.schnedlitz@gmail.com>
2021-08-14 13:37:19 +0200

    Remove folders

--- out/revealjs-starter.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicmV2ZWFsanMtc3RhcnRlci5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
